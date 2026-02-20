9e8934a6c2c4be3daed67c9dc3501c973ba856a9
Borna <borna.aminiraouf@gmail.com>
2026-02-20 10:48:06 +0330

    Final update

--- update.py
+++ update.py
@@ -1,18 +1,22 @@
 # This file is for getting inputs from users and update the board.
 # 🎉 Have fun coding
-bord = [[],[],[],[],[],[],[]]
-def bord_update (user_choice , turn , has_won):
-    if turn == 1:
+bord = [[], [], [], [], [], [], []]
+
+
+def bord_update(bord: list[list], user_choice: int, turn: int) -> list[list]:
+    if check_is_full(user_choice) == True:
+        print("This column is full, please choose another one.")
+        return True
+    elif turn == 1:
         bord[user_choice-1].append('X')
-    else:
+    elif turn == 2:
         bord[user_choice-1].append('O')
 
-def check_is_full(n : int):
-    if len(bord[n-1]) ==6:
-        return False
+    return bord
+
+
+def check_is_full(n: int) -> bool:
+    if len(bord[n-1]) == 6:
+        return True
     else:
-        return True    
-
-
-def game_status():
-    pass
+        return False
